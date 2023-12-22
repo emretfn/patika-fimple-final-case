68a0a25677acff0b59ac37d829e91b77614b6d5a
Emre Tufan <emretufan42@gmail.com>
2023-12-22 15:20:11 +0300

    feat: add a page banner component

---
+++ src/components/page-banner/PageBanner.tsx
@@ -0,0 +1,17 @@
+import styles from "./PageBanner.module.css";
+
+interface PageBannerProps {
+  title: string;
+  description: string;
+}
+
+export default function PageBanner({ title, description }: PageBannerProps) {
+  return (
+    <div className={styles.pageBanner}>
+      <div className={`${styles.pageCaption} container`}>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
