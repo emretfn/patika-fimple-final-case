8cddc2c61e0ac40351343e794c7d27902ce187de
Emre Tufan <emretufan42@gmail.com>
2023-12-22 15:52:05 +0300

    style: add logo to navbar and add favicon

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,9 @@ const Navbar = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link to={"/"}>Logo</Link>
+        <Link to={"/"}>
+          <img src="ticketease-logo.svg" alt="ticketease logo" />
+        </Link>
         <div className={styles.links}>
           <Link to={"/basvuru-olustur"}>Başvuru Oluştur</Link>
           <Link to={"/basvuru-sorgula"}>Başvuru Sorgula</Link>
