fbbb85bdb5675e6ac72912f7cf88275d38b982e4
Emre Tufan <emretufan42@gmail.com>
2023-12-26 02:16:11 +0300

    refactor: add min req. to password on admin login schema

--- src/lib/validations/admin-login-schema.ts
+++ src/lib/validations/admin-login-schema.ts
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const adminLoginSchema = yup.object().shape({
   username: yup.string().required("Kullanıcı adı zorunlu"),
-  password: yup.string().required("Şifre zorunlu"),
+  password: yup.string().required("Şifre zorunlu").min(6, "Şifre en az 6 karakter olmalı"),
 });
 
 export type AdminLoginType = yup.InferType<typeof adminLoginSchema>;
