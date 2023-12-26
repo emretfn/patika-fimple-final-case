8f662b28474e16d539349e8f415264b07da38c82
Emre Tufan <emretufan42@gmail.com>
2023-12-26 13:04:34 +0300

    refactor: add navbar admin check

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,35 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { useAuth } from "@/store/auth/hooks";
+import Button from "../ui/Button/Button";
+import clsx from "clsx";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store";
+import { signOut } from "@/store/auth/authThunk";
 
 const Navbar = () => {
+  const { userData } = useAuth();
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <header className={styles.header}>
-      <nav className={`${styles.nav} container`}>
-        <Link to={"/"}>
+      <nav className={clsx(styles.nav, "container")}>
+        <Link to={userData ? "/admin" : "/"}>
           <img src="/ticketease-logo.svg" alt="ticketease logo" />
         </Link>
-        <div className={styles.links}>
-          <Link to={"/basvuru-olustur"}>Başvuru Oluştur</Link>
-          <Link to={"/basvuru-sorgula"}>Başvuru Sorgula</Link>
-        </div>
+        {userData ? (
+          <div className={styles.links}>
+            <Link to={"/admin/basvuru-listesi"}>Başvuru Listesi</Link>
+            <Button variant="outline" onClick={() => dispatch(signOut())}>
+              Çıkış Yap
+            </Button>
+          </div>
+        ) : (
+          <div className={styles.links}>
+            <Link to={"/basvuru-olustur"}>Başvuru Oluştur</Link>
+            <Link to={"/basvuru-sorgula"}>Başvuru Sorgula</Link>
+          </div>
+        )}
       </nav>
     </header>
   );
