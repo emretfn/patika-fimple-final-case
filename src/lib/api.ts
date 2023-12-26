0758423bef85800a7f96ef198521b94bd6a23f1b
Emre Tufan <emretufan42@gmail.com>
2023-12-26 22:53:13 +0300

    fix: check if jwt expired then signout

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,23 @@
 import axios from "axios";
 import { API_URL } from "./config";
+import { store } from "@/store";
+import { signOut } from "@/store/auth/authThunk";
 
 const api = axios.create({
   baseURL: API_URL,
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      store.dispatch(signOut());
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
