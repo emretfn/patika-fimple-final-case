8baf84e410ada0aa61110b6b83f18fd438f27b7c
Emre Tufan <emretufan42@gmail.com>
2023-12-28 03:25:08 +0300

    refactor: change loading and error logic on admin ticket detail page

--- src/pages/admin-ticket-detail/AdminTicketDetailPage.tsx
+++ src/pages/admin-ticket-detail/AdminTicketDetailPage.tsx
@@ -11,17 +11,10 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
-// TODO: if ticket is not ready render loading
-export default function AdminTicketDetailPage() {
+function AdminTicketDetailPage() {
   const dispatch = useDispatch<AppDispatch>();
   const { ticketCode } = useParams();
   const { ticket, error, loading } = useTicketStore();
-
-  useEffect(() => {
-    if (error) {
-      throw new Error("404 Bulunamadı.");
-    }
-  }, [error]);
 
   useEffect(() => {
     if (ticketCode) {
@@ -29,43 +22,47 @@ export default function AdminTicketDetailPage() {
     }
   }, [ticketCode, dispatch]);
 
+  if (error && !ticket) {
+    throw new Error("404 Bulunamadı.");
+  }
+
+  if (loading) {
+    return <Spinner asOverlay />;
+  }
+
   return (
     <main>
       <PageBanner title={`Başvuru #${ticket?.ticketCode}`} description="" />
       <div className={clsx("container", styles.pageContainer)}>
         <Card>
           <CardHeader>Başvuran Bilgisi</CardHeader>
-          {loading ? (
-            <Spinner size={32} />
-          ) : (
-            <CardContent className={styles.applicantCardContent}>
-              <div className={styles.userDetail}>
-                <h5 className={styles.userName}>
-                  {ticket?.userName} {ticket?.userSurname}
-                </h5>
-                <p>Yaş: {ticket?.userAge}</p>
-                <p>TC: {ticket?.userTc}</p>
+          <CardContent className={styles.applicantCardContent}>
+            <div className={styles.userDetail}>
+              <h5 className={styles.userName}>
+                {ticket?.userName} {ticket?.userSurname}
+              </h5>
+              <p>Yaş: {ticket?.userAge}</p>
+              <p>TC: {ticket?.userTc}</p>
+            </div>
+            <div className={styles.applicationInfo}>
+              <p>
+                <b>Adres:</b> {ticket?.address}
+              </p>
+              <p>
+                <b>Başvuru nedeni:</b> {ticket?.reason}
+              </p>
+            </div>
+            {!!ticket?.attachments?.length && (
+              <div className={styles.uploadedFiles}>
+                <h6>Yüklenen Dosyalar</h6>
+                {ticket.attachments.map((fileUrl) => (
+                  <a href={fileUrl} key={fileUrl} target="_blank">
+                    File 1
+                  </a>
+                ))}
               </div>
-              <div className={styles.applicationInfo}>
-                <p>
-                  <b>Adres:</b> {ticket?.address}
-                </p>
-                <p>
-                  <b>Başvuru nedeni:</b> {ticket?.reason}
-                </p>
-              </div>
-              {!!ticket?.attachments?.length && (
-                <div className={styles.uploadedFiles}>
-                  <h6>Yüklenen Dosyalar</h6>
-                  {ticket.attachments.map((fileUrl) => (
-                    <a href={fileUrl} key={fileUrl} target="_blank">
-                      File 1
-                    </a>
-                  ))}
-                </div>
-              )}
-            </CardContent>
-          )}
+            )}
+          </CardContent>
         </Card>
         <Card>
           <CardHeader>Başvuruyu güncelle</CardHeader>
@@ -77,3 +74,5 @@ export default function AdminTicketDetailPage() {
     </main>
   );
 }
+
+export default AdminTicketDetailPage;
