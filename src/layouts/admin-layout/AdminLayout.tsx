b6a4d276561ffe3c62ae8bddd7257abef549b216
Emre Tufan <emretufan42@gmail.com>
2023-12-28 03:23:57 +0300

    chore: remove spinner from admin layout

--- src/layouts/admin-layout/AdminLayout.tsx
+++ src/layouts/admin-layout/AdminLayout.tsx
@@ -1,17 +1,11 @@
-import styles from "./AdminLayout.module.css";
 import Navbar from "@/components/navbar/Navbar";
-import Spinner from "@/components/ui/Spinner/Spinner";
 import { useAuth } from "@/store/auth/hooks";
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function AdminLayout() {
   const { token, loading } = useAuth();
 
-  if (loading) {
-    return <Spinner className={styles.pageSpinner} size={48} />;
-  }
-
-  if (!token) {
+  if (!loading && !token) {
     return <Navigate to={"/admin"} />;
   }
 
